a9ed9baa0a6f032a15b577863934c3da0a4f257a
Janluc <Janlucsaneaux@gmail.com>
2020-10-15 10:48:26 -0400

    Format code according to Linter

--- bubble.rb
+++ bubble.rb
@@ -1,37 +1,38 @@
 def bubble_sort(arr)
-  lengthofArr = arr.length - 1;
-  loop do 
+  length_of_arr = arr.length - 1
+  loop do
     swap = false
-     lengthofArr.times do |item|
-        if arr[item] > arr[item + 1] 
-          arr[item], arr[item + 1] = arr[item + 1], arr[item]
-          swap = true
-        end
+    length_of_arr.times do |item|
+      if arr[item] > arr[item + 1] 
+        arr[item], arr[item + 1] = arr[item + 1], arr[item]
+        swap = true
       end
-      break if not swap
-  end
-  puts arr   
-end
+    end
 
+    break if not swap
+  end
+  puts arr
+end
 
 arr = [49, 22, 1, 55, 104, 7]
 
 bubble_sort(arr)
 
-arr = ["hello", "hey", "hi"]
+arr = ['hello', 'hey', 'hi']
 
 def bubble_sort_by(arr)
-  lengthofArr = arr.length - 1;
-  loop do 
+  length_of_arr = arr.length - 1;
+  loop do
     swap = false
-     lengthofArr.times do |item|
-      value = yield(arr[item], arr[item + 1])
-        if value >= 1 
-          arr[item], arr[item + 1] = arr[item + 1], arr[item]
-          swap = true
-        end
+    length_of_arr.times do |item|
+    value = yield(arr[item], arr[item + 1])
+      if value >= 1 
+        arr[item], arr[item + 1] = arr[item + 1], arr[item]
+        swap = true
       end
-      break if not swap
+    end
+    
+    break if not swap
   end
   puts arr
 end
