e39ea1af13b3acd45546359b6efd5f349027af54
Janluc <Janlucsaneaux@gmail.com>
2020-10-15 10:56:09 -0400

    Format code according to Linter

--- bubble.rb
+++ bubble.rb
@@ -3,13 +3,13 @@ def bubble_sort(arr)
   loop do
     swap = false
     length_of_arr.times do |item|
-      if arr[item] > arr[item + 1] 
+      if arr[item] > arr[item + 1]
         arr[item], arr[item + 1] = arr[item + 1], arr[item]
         swap = true
       end
     end
 
-    break if not swap
+    break unless swap
   end
   puts arr
 end
@@ -18,21 +18,21 @@ arr = [49, 22, 1, 55, 104, 7]
 
 bubble_sort(arr)
 
-arr = ['hello', 'hey', 'hi']
+arr = %w(hello hey hi)
 
 def bubble_sort_by(arr)
-  length_of_arr = arr.length - 1;
+  length_of_arr = arr.length - 1
   loop do
     swap = false
     length_of_arr.times do |item|
-    value = yield(arr[item], arr[item + 1])
-      if value >= 1 
+      value = yield(arr[item], arr[item + 1])
+      if value >= 1
         arr[item], arr[item + 1] = arr[item + 1], arr[item]
         swap = true
       end
     end
-    
-    break if not swap
+
+    break unless swap
   end
   puts arr
 end
